a4508d7613727e0efd4a5c11df20c8d3a8c0fbc9
Takashi SASAKI <takashi316@gmail.com>
2020-09-03 16:16:16 +0900

    add 10 buttons by for a loop.

--- app/src/main/java/jp/ac/kawahara/t_sasaki/myapplication/DentakuActivity.java
+++ app/src/main/java/jp/ac/kawahara/t_sasaki/myapplication/DentakuActivity.java
@@ -3,12 +3,23 @@ package jp.ac.kawahara.t_sasaki.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class DentakuActivity extends AppCompatActivity {
+
+    LinearLayout buttonPadLinearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dentaku);
+        this.buttonPadLinearLayout =
+                findViewById(R.id.buttonPadLinearLayout);
+
+        for (int i = 0; i < 10; ++i) {
+            Button b = new Button(this.getApplicationContext());
+            buttonPadLinearLayout.addView(b);
+        }
     }
 }
